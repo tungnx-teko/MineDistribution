7d4c8be334c7836445bcaf4a49dc0045cbcd9b14
Nguyễn Xuân Tùng <tung.nx@teko.vn>
2020-07-22 09:00:20 +0700

    First commit

---
+++ Mine.framework/Headers/Mine.h
@@ -0,0 +1,18 @@
+//
+//  Mine.h
+//  Mine
+//
+//  Created by Tung Nguyen on 7/22/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Mine.
+FOUNDATION_EXPORT double MineVersionNumber;
+
+//! Project version string for Mine.
+FOUNDATION_EXPORT const unsigned char MineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Mine/PublicHeader.h>
+
+
